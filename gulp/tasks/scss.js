35f2a44e0fefa703a1cc4c22a81d04802fdcd649
gudovchshikov <gudovchshikov@icloud.com>
2023-04-27 11:20:47 +0500

    Upload Template

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,15 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+
+    return plugins.gulp.src(paths.build.sass)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(plugins.concat('main.css'))
+        .pipe(plugins.concat('main.min.css'))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(plugins.gulp.dest(paths.build.css))
+        .pipe(plugins.browserSync.stream());
+        
+}
